f652abe2a1f69b660f7ff2c92dde41d38c99da06
bhanu <bhanupratap.mnit@gmail.com>
2021-05-01 16:06:21 -0400

    gitgo used

--- cmd/get_cmd.py
+++ cmd/get_cmd.py
@@ -20,6 +20,17 @@ mBERT_ADDR = '/mnt/nfs/work1/mfiterau/brawat/multilingual/mBERT_pytorch'
 TBOARD_DIR = '/mnt/nfs/work1/mfiterau/brawat/scratch/multiLM/tboard/'
 EWC_DIR = '/mnt/nfs/work1/mfiterau/brawat/scratch/multiLM_lm/ewc_params'
 
+# The command is based on the command below:
+# python run_glue.py \
+#   --model_name_or_path bert-base-cased \
+#   --task_name $TASK_NAME \
+#   --do_train \
+#   --do_eval \
+#   --max_seq_length 128 \
+#   --per_device_train_batch_size 32 \
+#   --learning_rate 2e-5 \
+#   --num_train_epochs 3 \
+#   --output_dir /tmp/$TASK_NAME/
 
 print('--'*10+' CMD '+'--'*10)
 print(cmd)
